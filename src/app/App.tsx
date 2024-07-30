d62d4e220492c35171adea68710e73e68f3b3158
drdynscript <drdynscript@gmail.com>
2024-07-30 13:08:12 +0200

    Added filtering of curriculum

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,19 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css'
 import { CourseCard, CourseDetailsModal } from './components/courses';
 import { LearningLinesAsInlineList } from './components/learning-lines';
 import { Course } from '../types/courses'
 
+import dataCurricula from './data/curricula.json';
+import dataCurriculum202425 from './data/curriculum_2024_25.json';
 import dataCurriculum202426 from './data/curriculum_2024_26.json';
 import dataLearningLines from './data/learning_lines.json';
+import dataSpecializations from './data/specializations.json';
 
 function App() {
-  const [dataCurriculum] = useState(dataCurriculum202426);
+  const [dataCurriculum, setDataCurriculum] = useState(dataCurriculum202426);
+  const [dataFilteredCurriculum, setDataFilteredCurriculum] = useState(dataCurriculum);
+  const [selectedSpecialization, setSelectedSpecialization] = useState("all");
   const [isDetailsModalOpen, setCourseDetailsModalOpen] = useState<boolean>(false);
   const [selectedCourse, setSelectedCourse] = useState<Course | null | undefined>(null);
 
@@ -20,10 +25,40 @@ function App() {
   const handleCloseCourseDetailsModal = useCallback(() => {
     setCourseDetailsModalOpen(false);
   }, []);
+
+  const handleChangeCurriculum = useCallback((ev: any) => {
+    switch (ev.target.value) {
+      case "Curriculum 2024-26": default: setDataCurriculum(dataCurriculum202426); break;
+      case "Curriculum 2024-25": setDataCurriculum(dataCurriculum202425); break;
+    }
+  }, []);
+
+  const handleChangeSpecialization = useCallback((ev: any) => {
+    setSelectedSpecialization(ev.target.value);
+  }, []);
+
+  useEffect(() => {
+    const filteredCurriculum = { ...dataCurriculum};  
+
+    if (selectedSpecialization !== "all") {
+      filteredCurriculum.courses = filteredCurriculum.courses.filter((course) => course.specializationCode === selectedSpecialization || course.specializationCode === undefined);
+    }
+
+    setDataFilteredCurriculum(filteredCurriculum);
+  }, [dataCurriculum, selectedSpecialization]);
   
   return (
     <>
       <div className="App">
+        <div className={`flex flex-row items-center justify-end p-2`}>
+          <select onChange={(ev) => handleChangeCurriculum(ev)} className={`bg-gray-50 border border-ahs_blue-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-ahs_blue-500 dark:focus:border-ahs_blue-500 mx-2`}>
+            {dataCurricula.map((curriculum, index) => <option key={index} value={curriculum.label}>{curriculum.label}</option>)}
+          </select>
+          <select onChange={(ev) => handleChangeSpecialization(ev)} className={`bg-gray-50 border border-ahs_blue-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-ahs_blue-500 dark:focus:border-ahs_blue-500 mx-2`}>
+            <option value="all">Alle specialisaties</option>
+            {dataSpecializations.map((specialization) => <option key={specialization.code} value={specialization.code}>{specialization.name}</option>)}
+          </select>
+        </div>
         <CourseDetailsModal 
           isOpen={isDetailsModalOpen}
           onClose={handleCloseCourseDetailsModal}
@@ -49,7 +84,7 @@ function App() {
               <div className="header__period" style={{ gridColumnStart: "p6", gridColumnEnd: "span 1", gridRowStart: "per"}}>Periode 6</div>
               <div className="header__period" style={{ gridColumnStart: "p7", gridColumnEnd: "span 1", gridRowStart: "per"}}>Periode 7</div>
               <div className="header__period" style={{ gridColumnStart: "p8", gridColumnEnd: "span 1", gridRowStart: "per"}}>Periode 8</div>
-              {dataCurriculum && dataCurriculum.courses && dataCurriculum.courses.map((course) =>
+              {dataFilteredCurriculum && dataFilteredCurriculum.courses && dataFilteredCurriculum.courses.map((course) =>
                 <CourseCard key={course.id} data={{ id: course.id, name: course.name, subName: course.subName, period: course.period, semester: 1, learningLineCode: course.learningLineCode, credits: course.credits, contactHoursPerWeek: course.contactHoursPerWeek, lecturers: course.lecturers, specializationCode: course.specializationCode }} onCourseDetailsOpen={handleOpenCourseDetailsModal} />
               )}
             </div>
